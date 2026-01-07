d93e690c621a9eec0d378d19085c948b9aacaa62
heeyeon <asd95121@gmail.com>
2026-01-07 11:25:34 +0900

    학습: 숫자가 아님 (NaN) 구분 - Number.isNaN() 함수만 사용! (권장)

--- javascript/05-타입변환/실습.js
+++ javascript/05-타입변환/실습.js
@@ -135,6 +135,9 @@ const tempHexCode = '#' + red.toString(16).padStart(2, '0') + green.toString(16)
 //출력 결과(예시): '#ff8000'
 console.log(hexCode)
 
+
+
+
 // --------------------------------------------------------------------------
 // 숫자 → 불리언 변환
 // --------------------------------------------------------------------------
@@ -142,15 +145,30 @@ console.log(hexCode)
 // --------------------------------------------------------------------------
 
 // 출력 결과: 0 → false
+console.log(Boolean(10))
+console.log(Boolean(1))
+console.log(Boolean(0))
+console.log(Boolean(-1))
+console.log(Boolean(-10))
+console.log(Boolean(0.1))
+
+// 이중 부정(!!) 연산자 활용(Boolean() 대체)
+console.log(!!(10))
+console.log(!!(1))
+console.log(!!(0))
+console.log(!!(-1))
+console.log(!!(-10))
+console.log(!!(0.1))
 
 // 출력 결과: 1 → true
-
+console.log(!!(1))
 // 출력 결과: -1 → true
+console.log(!!(-1))
 
 // 출력 결과: 0.1 → true
-
+console.log(!!(0.1))
 // 출력 결과: -0.1 → true
-
+console.log(!!(-0.1))
 
 // --------------------------------------------------------------------------
 // 문자 → 불리언 변환
@@ -159,10 +177,19 @@ console.log(hexCode)
 // --------------------------------------------------------------------------
 
 // 출력 결과: '' → false
+console.log('')
+console.log(Boolean(''))
+console.log(!!'')
 
 // 출력 결과: ' ' → true
+console.log(' ')
+console.log(Boolean(' '))
+console.log(!!' ')
 
 // 출력 결과: '0' → true
+console.log('0')
+console.log(Boolean('0'))
+console.log(!!'0')
 
 
 // --------------------------------------------------------------------------
@@ -178,22 +205,35 @@ console.log(hexCode)
 // --------------------------------------------------------------------------
 
 // 출력 결과: Math.sqrt(-1) → NaN
+console.log(Math.sqrt(-1))
 
 // 출력 결과: 숫자 + NaN → NaN
+console.log(2 + parseFloat('the load'))
 
 // 출력 결과: undefined + undefined → NaN
+console.log(Number(undefined))
 
+let t, u
+console.log(t+u)    
+  
 // 출력 결과: 문자 / 숫자 → NaN
+console.log('320px' / 4)
+console.log(parseInt('320px',10) / 4 + 'px')
 
 // 출력 결과: Number(str) → NaN
+console.log(Number('수요일'))
 
 // 출력 결과: parseInt(str) → NaN
+console.log(parseInt('오전 11시', 10))
 
 // 출력 결과: parseFloat(str) → NaN
+console.log(parseFloat('배터리 방전'))
 
 // 출력 결과: 0 / 0 → NaN
+console.log(0 / 0)
 
 // 출력 결과: Infinity - Infinity → NaN
+console.log(Infinity - Infinity)
 
 
 // --------------------------------------------------------------------------
@@ -204,29 +244,50 @@ console.log(hexCode)
 // * Number.isNaN(): 진짜 NaN만 판단 (정확) ✅
 // --------------------------------------------------------------------------
 
+// 비교 연산자로 NaN을 구분하는 것은 불가능
+const thisIsNaN = Math.sqrt(-1)
+console.log(thisIsNaN)
+
+console.log(thisIsNaN == NaN)
+console.log(thisIsNaN === NaN)
+
 // 출력 결과: typeof NaN → 'number' (⚠️ NaN이지만, number 타입으로 인식)
+console.log(typeof thisIsNaN)
 
 // 출력 결과: isNaN(1) → false (✅ NaN 아님)
+console.log(isNaN(1))
 
 // 출력 결과: isNaN('3') → false (✅ NaN 아님)
+console.log(isNaN('3'))
 
 // 출력 결과: isNaN([]) → false (✅ NaN 아님)
+console.log(isNaN([]))
 
 // 출력 결과: isNaN(null) → false (✅ NaN 아님)
+console.log(isNaN(null))
 
 // 출력 결과: isNaN(NaN) → true (✅ NaN 맞음)
+console.log(isNaN(NaN))
 
 // 출력 결과: isNaN('3n') → true (⚠️ NaN 맞음? 부정확한 이유: 타입 변환 후, 검사)
+console.log(isNaN('3n'))
+
 
 // 출력 결과: isNaN(undefined) → true (⚠️ NaN 맞음?)
+console.log(isNaN(undefined))
 
 // 출력 결과: isNaN({}) → true (⚠️ NaN 맞음?)
+console.log(isNaN({}))
+
 
 // 출력 결과: Number.isNaN('3n') → false (✅ NaN 아님, 정확한 이유: 타입 변환 없이 검사)
+console.log(Number.isNaN('3n'))
 
 // 출력 결과: Number.isNaN(undefined) → false (✅ NaN 아님)
+console.log(Number.isNaN(undefined))
 
 // 출력 결과: Number.isNaN({}) → false (✅ NaN 아님)
+console.log(Number.isNaN({}))
 
 
 // --------------------------------------------------------------------------
--- javascript/test.js
+++ javascript/test.js
@@ -51,3 +51,27 @@ const actorName = '김우빈'
 const actressName = '신민아'
 
 console.log(actorName > actressName)
+
+// 색상 값(일상, 10진수) → 색상 코드(컴퓨터, 16진수) 변환
+const red = 255, green = 120, blue = 30
+
+let hexCode = '#'
+const redHexValue = red.toString(16).padStart(2, '0')
+console.log(redHexValue)
+const greenHexValue = green.toString(16).padStart(2, '0')
+console.log(greenHexValue)
+const blueHexValue = blue.toString(16).padStart(2, '0')
+console.log(blueHexValue)
+
+hexCode += redHexValue
+hexCode += greenHexValue
+hexCode += blueHexValue
+
+
+let redColor = red.toString(16).padStart(2,'0') 
+let greenColor = green.toString(16).padStart(2,'0') 
+let blueColor = blue.toString(16).padStart(2, '0')
+
+const tempHexCode = '#' + redColor + greenColor + blueColor
+
+console.log(hexCode)
