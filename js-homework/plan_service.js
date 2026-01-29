2c12741eae1f6fac5e9c2b434bd0d260fafb9633
heeyeon <asd95121@gmail.com>
2026-01-30 01:32:22 +0900

    추가: 플랜서비스 자바스크립트 파일 추가, 박스 선택모델 선언

---
+++ js-homework/plan_service.js
@@ -0,0 +1,10 @@
+const planContainerBox = document.querySelectorAll('[data-label="planSelect"]')
+// console.log(planContainerBox)
+
+const basicPlan = document.querySelector('.free')
+const standardPlan = document.querySelector('.standard')
+const premiumPlan = document.querySelector('.premium')
+
+console.log(basicPlan)
+console.log(standardPlan)
+console.log(premiumPlan)
