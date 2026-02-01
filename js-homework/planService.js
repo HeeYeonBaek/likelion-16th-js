db15b6e136d305904de7f676b3ee78c452adde6a
heeyeon <asd95121@gmail.com>
2026-02-01 23:48:09 +0900

    추가 : 체크마크 영역

--- js-homework/planService.js
+++ js-homework/planService.js
@@ -1,27 +1,21 @@
 const planContainerBox = document.querySelectorAll('[data-label="planSelect"]')
 // console.log(planContainerBox)
+const innerButton = document.querySelectorAll('.clicker')
 const planComment = document.querySelector('.commentBox')
-
 const basicPlan = document.querySelector('.free')
 const standardPlan = document.querySelector('.standard')
 const premiumPlan = document.querySelector('.premium')
 
 
 const selectBox = () => {
-  planContainerBox.forEach((box) => {
-    if (planContainerBox[1]) {
-      box.addEventListener('click', (e, comment) => {
+  innerButton.forEach((box) => {
+    box.addEventListener('click', (e) => {
       e.currentTarget.style.cssText = `
         background: var(--active-box);
-        inline-block: 24px;
         border: 3px solid var(--active-color);
       `
-      comment.currentTarget.textComment = '플랜' 
-      })
-    }
-
-  })
-}
+    }) 
+  })    
+}       
 
 selectBox()
-
