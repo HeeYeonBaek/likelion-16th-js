689944bfd0aa04742772a46c617621bea3f1e7d6
heeyeon <asd95121@gmail.com>
2026-02-05 13:07:26 +0900

    폼필드선택 백업, 폼필드이벤트 코드 추가

--- javascript/63-폼-필드-선택/learn.js
+++ javascript/63-폼-필드-선택/learn.js
@@ -50,3 +50,32 @@
 // - 데이터 구조가 중요하다면? -> name
 // - 특정 고유 요소가 필요하다면? -> id (대괄호 표기 주의)
 // --------------------------------------------------------------------------
+
+// 실습: 로그인 폼
+;(() => {
+
+  // 로그인 폼에 사용자가 입력한 아이디, 비밀번호 값을 읽어와 
+  // 콘솔 패널에 출력합니다. (form.elements 활용)
+
+  const form = document.getElementById('login-form')
+  
+  form.addEventListener('submit', (e) => {
+    // 브라우저 기본 작동 방지
+    e.preventDefault()
+
+    // 폼 컨트롤 콜렉션 접근
+    const formControls = form.elements
+    // 아이디 컨트롤 값
+    const { value: idValue } = formControls.userid
+    // 패스워드 컨트롤 값
+    const { value: passwordValue } = formControls.password
+
+    // 콘솔 패널에 출력
+    console.log({ idValue, passwordValue })
+
+    // 폼 초기화
+    const formElement = e.currentTarget
+    formElement.reset()
+  })
+  
+})()
